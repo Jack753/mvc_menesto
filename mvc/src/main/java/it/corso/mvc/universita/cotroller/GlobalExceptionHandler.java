8150b23a8de4b41c95e79db102709af676e9d2eb
CORSO 8 <CORSO 8@192.168.104.17>
2018-02-14 11:28:30 +0100

    ass

--- mvc/src/main/java/it/corso/mvc/universita/cotroller/GlobalExceptionHandler.java
+++ mvc/src/main/java/it/corso/mvc/universita/cotroller/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package it.corso.mvc.universita.cotroller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +11,9 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+	private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+			
+			
 	@ExceptionHandler(Exception.class)
 	public ModelAndView handleException(HttpServletRequest request, Exception ex){
 		ModelAndView modelAndView = new ModelAndView();
